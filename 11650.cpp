c210db4fb4b3791436f60955b799ff8a9175daed
Yebin Kim <mareng825@naver.com>
2019-09-16 14:17:37 +0900

    Sorting Coordinate
    https://www.acmicpc.net/problem/11650
    
    My solution for sorting coordinate by using vector, pair, and STL sort().

---
+++ 11650.cpp
@@ -0,0 +1,29 @@
+//11650. 좌표 정렬하기
+
+
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	int n, x, y;
+	cin >> n;
+
+	vector<pair<int, int>> v;
+
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		v.push_back(pair<int, int>(x, y));
+	}
+
+	sort(v.begin(), v.end());
+
+	for (int i = 0; i < n; i++) {
+		cout << v[i].first << " " << v[i].second << '\n';
+	}
+
+	return 0;
+}
