fceac7ff01443233a2545edb92072e25775909ce
Yebin Kim <mareng825@naver.com>
2019-09-28 01:50:30 +0900

    Uploaded 11403.cpp - Used Floyd-Warshall Algorithm

---
+++ BaekJoon/11403.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int graph[101][101];
+	int n;
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> graph[i][j];
+		}
+	}
+
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (graph[i][k] && graph[k][j]) {
+					graph[i][j] = 1;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << graph[i][j] <<' ';
+		}
+		cout << '\n';
+	}
+	return 0;
+}
