b5b985da724d8d68f77307a37e85996db4f22783
Yebin Kim <mareng825@naver.com>
2019-10-18 01:52:39 +0900

    Updated 2156.cpp - Used Dynamic Programming

--- BaekJoon/2156.cpp
+++ BaekJoon/2156.cpp
@@ -11,20 +11,20 @@ int main() {
 
 	cin >> n;
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 1; i <= n; i++) {
 		cin >> wine[i];
 	}
 
-	dp[0] = wine[0];					//initialization
-	dp[1] = dp[0] + wine[1];
-	dp[2] = max(dp[1], dp[0] + dp[2]);
+	dp[0] = 0; //initialization
+	dp[1] = wine[1];
+	dp[2] = dp[1] + wine[2];
 
-	for (int i = 3; i < n; i++) { //get max
-		dp[i] = max(dp[i - 3] + wine[i - 1] + wine[i], dp[i - 2] + wine[i]);
+	for (int i = 3; i <= n; i++) { //get max
+		dp[i] = max(dp[i - 3] + wine[i - 1] + wine[i], dp[i - 2] + wine[i]); //if wine[i] included
+		dp[i] = max(dp[i], dp[i - 1]); //if wine[i] not included
 	}
 
-	sort(dp, dp + n);
-	cout << dp[n - 1];
+	cout << dp[n];
 	
 	return 0;
 }
