4cc4897073eed6d9b7eb7b4e66a31123f3613812
Yebin Kim <mareng825@naver.com>
2019-11-20 01:38:40 +0900

    Uploaded 2019_1_오픈채팅방.cpp

---
+++ Kakao/2019_1_오픈채팅방.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+
+	vector<string> record;
+	vector<string> answer;
+	map<string, string> m;
+
+	for (int i = 0; i < record.size(); i++) {
+		int cmdIndex = record[i].find(' ');
+		string cmd = record[i].substr(0, cmdIndex); //get cmd
+
+		record[i] = record[i].substr(cmdIndex + 1);
+		int uidIndex = record[i].find(' ');
+
+		string uid, name;
+		if (uidIndex < 0) uid = record[i]; //separate uid and name
+		else {
+			uid = record[i].substr(0, uidIndex);
+			name = record[i].substr(uidIndex + 1);
+
+			m[uid] = name;
+		}
+
+		if (cmd == "Enter") {
+			answer.push_back(uid + " ´ÔÀÌ µé¾î¿Ô½À´Ï´Ù.");
+		}
+		else if (cmd == "Leave") {
+			answer.push_back(uid + " ´ÔÀÌ ³ª°¬½À´Ï´Ù.");
+		}
+	}
+
+	for (int i = 0; i < answer.size(); i++) { //change to matched name
+		int nameIndex = answer[i].find(' ');
+		string id = answer[i].substr(0, nameIndex);
+		answer[i] = answer[i].substr(nameIndex + 1);
+		answer[i] = m[id] + answer[i];
+	}
+
+	return 0;
+}
